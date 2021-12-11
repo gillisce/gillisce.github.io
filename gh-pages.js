751b7e2e1ff8e3a9bd51475da91cbbfa82afd8a2
Chris Gillis <cgillis93@gmail.com>
2021-12-11 10:40:05 -0500

    Inti SvelteKit

--- gh-pages.js
+++ gh-pages.js
@@ -1,16 +1,17 @@
-var ghpages = require('gh-pages');
+import { publish } from 'gh-pages';
 
-ghpages.publish(
-    'public', // path to public directory
-    {
-        branch: 'gh-pages',
-        repo: 'https://github.com/gillisce/gillisce.github.io.git', // Update to point to your repository  
-        user: {
-            name: 'Chris Gillis', // update to use your name
-            email: 'cgillis93@gmail.com' // Update to use your email
-        }
-    },
-    () => {
-        console.log('Deploy Complete!')
-    }
-)
+publish(
+ 'build', // path to public directory
+ {
+  branch: 'gh-pages',
+  repo: 'https://github.com/gillisce/gillisce.github.io.git', // Update to point to your repository
+  user: {
+   name: 'CGillis', // update to use your name
+   email: 'cgillis93@gmail.com' // Update to use your email
+  },
+  dotfiles: true
+  },
+  () => {
+   console.log('Deploy Complete!');
+  }
+);
